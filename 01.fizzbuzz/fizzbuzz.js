26497026ceafa7d9e702a16132ffd7515f55023d
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-07-09 11:51:28 +0900

    ・メモリ・実行効率の改善

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,11 @@
-const range = [...Array(20)].map((_, i) => i + 1) // 1~20
-
-range.forEach(n => {
-  if (n % 3 === 0 && n % 5 === 0) {
+for(let i=1; i<21; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz')
-  } else if (n % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log('Fizz')
-  } else if (n % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log('Buzz')
   } else {
-    console.log(n)
+    console.log(i)
   }
-})
+}
