a4a18d192fc3377a1edd3cee29bac5766f8d77ee
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-07-26 13:19:51 +0900

    リファクタリング：非同期処理の簡素化

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -2,39 +2,31 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./db/memo.db')
 
 class Memo {
-  static open () {
-    return new Promise(resolve => {
-      db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)')
-      resolve()
-    })
+  static async open () {
+    db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)')
   }
 
   static all () {
     return new Promise(resolve => {
-      db.all('SELECT * FROM memos', (_, rows) => {
-        resolve(rows)
-      })
+      db.all('SELECT * FROM memos', (_, rows) => resolve(rows))
     })
   }
 
   static save (data) {
-    return new Promise(resolve => {
+    return new Promise(() => {
       db.run('INSERT INTO memos(text) VALUES(?)', data)
-      resolve()
     })
   }
 
   static destroy (id) {
-    return new Promise(resolve => {
+    return new Promise(() => {
       db.run('DELETE FROM memos WHERE id = ?', id)
-      resolve()
     })
   }
 
   static close () {
     return new Promise(resolve => {
       db.close()
-      resolve()
     })
   }
 }
