8b0e4c220eaac10de4722273f4dc2c5a32f84167
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-08-01 20:52:21 +0900

    リファクタリング：取得したデータベースをインスタンス変数化

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -1,32 +1,32 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./db/memo.db')
 
 class Memo {
   static async open () {
-    db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)')
+    this.db = new sqlite3.Database('./db/memo.db')
+    this.db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)')
   }
 
   static all () {
     return new Promise(resolve => {
-      db.all('SELECT * FROM memos', (_, rows) => resolve(rows))
+      this.db.all('SELECT * FROM memos', (_, rows) => resolve(rows))
     })
   }
 
   static save (data) {
     return new Promise(() => {
-      db.run('INSERT INTO memos(text) VALUES(?)', data)
+      this.db.run('INSERT INTO memos(text) VALUES(?)', data)
     })
   }
 
   static destroy (id) {
     return new Promise(() => {
-      db.run('DELETE FROM memos WHERE id = ?', id)
+      this.db.run('DELETE FROM memos WHERE id = ?', id)
     })
   }
 
   static close () {
     return new Promise(resolve => {
-      db.close()
+      this.db.close()
     })
   }
 }
