f1ce2d11e1ea6c224fbd578906b13f38b2b71515
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-08-01 20:14:43 +0900

    Main関数内のメモ関連処理をクラス化

--- 04.class/exec-memo.js
+++ 04.class/exec-memo.js
@@ -3,57 +3,83 @@ const fs = require('fs')
 const SelectWithDiscription = require('./lib/prompt/select-with-discription.js')
 const Memo = require('./lib/memo.js')
 
-const main = async () => {
-  await Memo.open()
+class MemoExcutor {
+  static async build () {
+    await Memo.open()
+  }
 
-  if (opts.l) {
+  static async destroy () {
+    await Memo.close()
+  }
+
+  static async showMemoList () {
     const memos = await Memo.all()
 
     for (const idx in memos) {
       const line = memos[idx].text.split('\n')[0]
       console.log(line)
     }
-  } else if (opts.r) {
+  }
+
+  static async showMemoReview () {
     const memos = await Memo.all()
     if (memos.length === 0) return
 
-    const prompt = selectPrompt(memos, { optMsg: 'see' })
+    const prompt = this.#selectPrompt(memos, { optMsg: 'see' })
     const idx = await prompt.run()
 
     const memo = memos[idx]
     console.log(`\n${memo.text}`)
-  } else if (opts.d) {
+  }
+
+  static async deleteMemo () {
     const memos = await Memo.all()
     if (memos.length === 0) return
 
-    const prompt = selectPrompt(memos, { optMsg: 'delete' })
+    const prompt = this.#selectPrompt(memos, { optMsg: 'delete' })
     const idx = await prompt.run()
 
     await Memo.destroy(memos[idx].id)
-  } else {
+  }
+
+  static async createMemo () {
     const inputData = fs.readFileSync('/dev/stdin', 'utf8')
     await Memo.save(inputData)
   }
 
-  await Memo.close()
+  static #selectPrompt (memos, { optMsg }) {
+    const data = []
+    for (const idx in memos) {
+      const lines = memos[idx].text.split('\n')
+      data.push(
+        {
+          name: lines[0],
+          discription: lines.slice(1).join('\n')
+        })
+    }
+
+    return new SelectWithDiscription({
+      name: 'Choice',
+      message: `Choose a note you want to ${optMsg}:`,
+      choices: data
+    })
+  }
 }
 
-const selectPrompt = (memos, { optMsg }) => {
-  const data = []
-  for (const idx in memos) {
-    const lines = memos[idx].text.split('\n')
-    data.push(
-      {
-        name: lines[0],
-        discription: lines.slice(1).join('\n')
-      })
+const main = async () => {
+  MemoExcutor.build()
+
+  if (opts.l) {
+    await MemoExcutor.showMemoList()
+  } else if (opts.r) {
+    await MemoExcutor.showMemoReview()
+  } else if (opts.d) {
+    await MemoExcutor.deleteMemo()
+  } else {
+    await MemoExcutor.createMemo()
   }
 
-  return new SelectWithDiscription({
-    name: 'Choice',
-    message: `Choose a note you want to ${optMsg}:`,
-    choices: data
-  })
+  MemoExcutor.destroy()
 }
 
 main()
