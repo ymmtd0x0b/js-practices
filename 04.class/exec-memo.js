0f9c51dba082770075f77d7d3f03ae1f443ad04c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-08-02 12:11:55 +0900

    リファクタリング：メソッド名の修正

--- 04.class/exec-memo.js
+++ 04.class/exec-memo.js
@@ -1,85 +1,22 @@
 const opts = require('minimist')(process.argv)
 const fs = require('fs')
-const SelectWithDiscription = require('./lib/prompt/select-with-discription.js')
-const Memo = require('./lib/memo.js')
-
-class MemoExcutor {
-  static async build () {
-    await Memo.open()
-  }
-
-  static async destroy () {
-    await Memo.close()
-  }
-
-  static async showMemoList () {
-    const memos = await Memo.all()
-
-    for (const idx in memos) {
-      const line = memos[idx].text.split('\n')[0]
-      console.log(line)
-    }
-  }
-
-  static async showMemoReview () {
-    const memos = await Memo.all()
-    if (memos.length === 0) return
-
-    const prompt = this.#selectPrompt(memos, { optMsg: 'see' })
-    const idx = await prompt.run()
-
-    const memo = memos[idx]
-    console.log(`\n${memo.text}`)
-  }
-
-  static async deleteMemo () {
-    const memos = await Memo.all()
-    if (memos.length === 0) return
-
-    const prompt = this.#selectPrompt(memos, { optMsg: 'delete' })
-    const idx = await prompt.run()
-
-    await Memo.destroy(memos[idx].id)
-  }
-
-  static async createMemo () {
-    const inputData = fs.readFileSync('/dev/stdin', 'utf8')
-    await Memo.save(inputData)
-  }
-
-  static #selectPrompt (memos, { optMsg }) {
-    const data = []
-    for (const idx in memos) {
-      const lines = memos[idx].text.split('\n')
-      data.push(
-        {
-          name: lines[0],
-          discription: lines.slice(1).join('\n')
-        })
-    }
-
-    return new SelectWithDiscription({
-      name: 'Choice',
-      message: `Choose a note you want to ${optMsg}:`,
-      choices: data
-    })
-  }
-}
+const MemoExecutor = require('./lib/memo-executor.js')
 
 const main = async () => {
-  MemoExcutor.build()
+  MemoExecutor.build()
 
   if (opts.l) {
-    await MemoExcutor.showMemoList()
+    await MemoExecutor.showList()
   } else if (opts.r) {
-    await MemoExcutor.showMemoReview()
+    await MemoExecutor.showReview()
   } else if (opts.d) {
-    await MemoExcutor.deleteMemo()
+    await MemoExecutor.delete()
   } else {
-    await MemoExcutor.createMemo()
+    const inputData = fs.readFileSync('/dev/stdin', 'utf8')
+    await MemoExecutor.add(inputData)
   }
 
-  MemoExcutor.destroy()
+  MemoExecutor.destroy()
 }
 
 main()
---
+++ 04.class/lib/memo-executor.js
@@ -0,0 +1,66 @@
+const SelectWithDiscription = require('./prompt/select-with-discription.js')
+const Memo = require('./memo.js')
+
+class MemoExecutor {
+  static async build () {
+    await Memo.open()
+  }
+
+  static async destroy () {
+    await Memo.close()
+  }
+
+  static async showList () {
+    const memos = await Memo.all()
+
+    for (const idx in memos) {
+      const line = memos[idx].text.split('\n')[0]
+      console.log(line)
+    }
+  }
+
+  static async showReview () {
+    const memos = await Memo.all()
+    if (memos.length === 0) return
+
+    const prompt = this.#selectPrompt(memos, { optMsg: 'see' })
+    const idx = await prompt.run()
+
+    const memo = memos[idx]
+    console.log(`\n${memo.text}`)
+  }
+
+  static async delete () {
+    const memos = await Memo.all()
+    if (memos.length === 0) return
+
+    const prompt = this.#selectPrompt(memos, { optMsg: 'delete' })
+    const idx = await prompt.run()
+
+    await Memo.destroy(memos[idx].id)
+  }
+
+  static async add (data) {
+    await Memo.save(data)
+  }
+
+  static #selectPrompt (memos, { optMsg }) {
+    const data = []
+    for (const idx in memos) {
+      const lines = memos[idx].text.split('\n')
+      data.push(
+        {
+          name: lines[0],
+          discription: lines.slice(1).join('\n')
+        })
+    }
+
+    return new SelectWithDiscription({
+      name: 'Choice',
+      message: `Choose a note you want to ${optMsg}:`,
+      choices: data
+    })
+  }
+}
+
+module.exports = MemoExecutor
