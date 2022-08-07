5864d5036e2f4127e68efe0c079b9a1cd2d49b02
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-08-08 04:24:39 +0900

    拒否の取得方法の修正

--- 04.class/exec-memo.js
+++ 04.class/exec-memo.js
@@ -5,19 +5,21 @@ const MemoExecutor = require('./lib/memo/memo-executor.js')
 const main = async () => {
   await MemoExecutor.build()
 
-  try {
-    if (opts.l) {
-      await MemoExecutor.showList()
-    } else if (opts.r) {
-      await MemoExecutor.showReview()
-    } else if (opts.d) {
-      await MemoExecutor.delete()
-    } else {
-      const inputData = fs.readFileSync('/dev/stdin', 'utf8')
-      await MemoExecutor.add(inputData)
-    }
-  } catch (e) {
-    console.log(e.message)
+  if (opts.l) {
+    await MemoExecutor.showList().catch(err => {
+      console.log(err.message)
+    })
+  } else if (opts.r) {
+    await MemoExecutor.showReview().catch(err => {
+      console.log(err.message)
+    })
+  } else if (opts.d) {
+    await MemoExecutor.delete().catch(err => {
+      console.log(err.message)
+    })
+  } else {
+    const inputData = fs.readFileSync('/dev/stdin', 'utf8')
+    await MemoExecutor.add(inputData)
   }
 
   await MemoExecutor.destroy()
