d973ee8b78586ea878e406aba212d3bbf0eeb122
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-08-11 04:04:44 +0900

    resolveが意図通りに実行されるように修正

--- 04.class/lib/memo/memo.js
+++ 04.class/lib/memo/memo.js
@@ -4,8 +4,7 @@ class Memo {
   static async open () {
     return new Promise(resolve => {
       this.db = new sqlite3.Database('./db/memo.db')
-      this.db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)')
-      resolve()
+      this.db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER primary key, text TEXT)', () => resolve())
     })
   }
 
@@ -17,22 +16,19 @@ class Memo {
 
   static save (data) {
     return new Promise(resolve => {
-      this.db.run('INSERT INTO memos(text) VALUES(?)', data)
-      resolve()
+      this.db.run('INSERT INTO memos(text) VALUES(?)', data, () => resolve())
     })
   }
 
   static destroy (id) {
     return new Promise(resolve => {
-      this.db.run('DELETE FROM memos WHERE id = ?', id)
-      resolve()
+      this.db.run('DELETE FROM memos WHERE id = ?', id, () => resolve())
     })
   }
 
   static close () {
     return new Promise(resolve => {
-      this.db.close()
-      resolve()
+      this.db.close(() => resolve())
     })
   }
 }
